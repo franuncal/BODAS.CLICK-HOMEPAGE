f884c6a51427675664759a39779d75144bfad8af
Francisco Uncal <franciscouncal@Fran-MacBook-Pro.local>
2024-07-31 18:58:39 -0300

    Agregando repo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  document.querySelector(".modal-content").classList.add("show");
+};
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function () {
+  modal.style.display = "none";
+  document.querySelector(".modal-content").classList.remove("show");
+};
+
+// Cuando el usuario hace clic fuera del modal, cierra el modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.querySelector(".modal-content").classList.remove("show");
+  }
+};
